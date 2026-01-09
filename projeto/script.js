ca4218de1b25ccfed568fe6b04c34d16f1399a49
Maria Aparecida <aparecida.duarte@academico.ifpb.edu.br>
2026-01-09 20:23:48 -0300

    adicionando o data-category para ajustar  os filtros por categoria

--- projeto/script.js
+++ projeto/script.js
@@ -1,14 +1,36 @@
+// contador do carrinho
+let totalItens = 0;
 
-let contador = 0;
-
-const contadorSpan = document.getElementById("contador-carrinho");
 const botoesComprar = document.querySelectorAll(".btn-comprar");
 
-botoesComprar.forEach(botao => {
+const contadorCarrinho = document.querySelector(".contador-carrinho");
+
+botoesComprar.forEach((botao) => {
   botao.addEventListener("click", () => {
-    contador++;
-    contadorSpan.textContent = contador;
+    totalItens++;
+
+    contadorCarrinho.textContent = totalItens;
 
     alert("✅ Produto adicionado ao carrinho!");
+  });
+});
+
+// filtro de produtos por categoria att2.0 oficial
+const filtros = document.querySelectorAll(".filtros a");
+const produtos = document.querySelectorAll(".produto");
+
+filtros.forEach(filtro => {
+  filtro.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const categoria = filtro.getAttribute("data-filter");
+
+    produtos.forEach(produto => {
+      if (categoria === "todos" || produto.dataset.category === categoria) {
+        produto.style.display = "block";
+      } else {
+        produto.style.display = "none";
+      }
+    });
   });
 });
